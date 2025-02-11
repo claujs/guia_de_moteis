3f6bb9f56ba7665271e52cc10566fa14dd0ed12a
Cláudio Silva <70970841+claujs@users.noreply.github.com>
2025-02-11 17:28:43 -0300

    Update guia_de_moteis_page.dart

--- lib/presentation/pages/guia_de_moteis_page.dart
+++ lib/presentation/pages/guia_de_moteis_page.dart
@@ -1,4 +1,3 @@
-// lib/presentation/pages/guia_de_moteis_list_page.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:guia_moteis/presentation/widgets/guia_de_moteis_body.dart';
